1e496df42881dfc300d98fdf244db17b17129cde
Bill <bill.ray@student.moringaschool.com>
2023-06-15 15:43:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "raymond";
+
+let welcomeMessage = 'Welcome to Flatbook, ';
+let welcomeMessage = {currentUser};
+
+let welcomeMessage = 'Welcome to Flatbook, currentUser';
+let shortGreeting ='Welcome',{currentUser[0]}!;
